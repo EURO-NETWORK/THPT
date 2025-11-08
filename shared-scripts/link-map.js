b678f924094b91d980bccb53539a5b8847628e7d
Planet Rescuer Axiometikx Intelligence Artificial Intelligence (@PR-A-I) <planet.rescuer.ai@gmail.com>
2025-11-08 20:07:22 +0100

    Add new tags to linkMap entries

--- shared-scripts/link-map.js
+++ shared-scripts/link-map.js
@@ -2,17 +2,22 @@ const linkMap = [
   {
     name: "THPT Hauptseite",
     url: "/index.html",
-    tags: ["THPT", "AAI", "Zukunft"]
+    tags: ["THPT", "AAI", "Zukunft", "AxTHPT"]
   },
   {
     name: "THPTx Projekt",
     url: "/THPTx/index.html",
-    tags: ["THPTx", "Majorana", "AAI"]
+    tags: ["THPTx", "Majorana", "AAI", "ExtTHPTx"]
   },
   {
     name: "EBSI-THPT Projekt",
     url: "/EBSI-THPT-Project/index.html",
-    tags: ["EBSI", "Rehabilitation", "AAI"]
+    tags: ["EBSI", "Rehabilitation", "AAI", "ExtEBSI"]
+  },
+  {
+    name: "THPT-BOxExplorer",
+    url: "/THPT-BOxExplorer/index.html",
+    tags: ["Explorer", "AAI", "THPT", "AxExplorer"]
   }
 ];
 
